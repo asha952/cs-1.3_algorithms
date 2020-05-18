950c34b2db4430f51224c1d966ec5fcb0f8315f0
Ashab_Siddiqui <ashabsiddiqui952@gmail.com>
2020-05-17 21:34:53 -0700

    [added] fixed stack.py

--- stack.py
+++ stack.py
@@ -19,10 +19,7 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        if self.list.size == 0:
-            return True
-        else:
-            return False
+        return self.list.size == 0
 
     def length(self):
         """Return the number of items in this stack."""
@@ -38,17 +35,20 @@ class LinkedStack(object):
         or None if this stack is empty."""
         if self.is_empty():
             return None
-        return self.list.tail.data
+        else:
+            return self.list.tail.data
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? """
-        if self.list.tail is None:
-            raise ValueError('Nothing to pop')
-        popped_data = self.list.tail.data
-        self.list.delete(popped_data)  # delete tail
-        return popped_data
+        if self.is_empty():
+            raise ValueError("Empty Stack")
+        else:
+            item = self.list.head.data
+            self.list.head = self.list.head.next
+            self.list.size -= 1
+            return item
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
@@ -69,7 +69,9 @@ class ArrayStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        return self.length() == 0
+        if not self.list:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this stack."""
@@ -83,20 +85,21 @@ class ArrayStack(object):
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        if self.length() == 0:
+        if self.is_empty():
             return None
-        return self.list[self.length() - 1]
+        else:
+            return self.list[len(self.list) - 1]
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? """
-        if self.length() == 0:
-            raise ValueError('Nothing to pop')
-        popped_data = self.peek()
-        self.list.pop()
-        # self.list.delete(popped_data) #delete tail
-        return popped_data
+        if self.is_empty():
+            raise ValueError("Empty Stack")
+        else:
+            item = self.peek()
+            self.list.pop(len(self.list) - 1)
+            return item
 
 
 # Implement LinkedStack and ArrayStack above, then change the assignment below
